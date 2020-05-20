867d2d700a9c6d3b2034827fe7bd2f380646cd08
xzh1999 <591264996@qq.com>
2020-05-20 20:58:03 +0800

    add autotest code

---
+++ src/baidu_ui/const.py
@@ -0,0 +1,3 @@
+# coding = utf-8
+
+EXCEL_DIR="D:/pytest/data/search_query.xlsx"
---
+++ src/httpbin_api/const.py
@@ -0,0 +1,6 @@
+# coding = utf-8
+BASE_URL="http://httpbin.org/"
+#BASE_URL="192.168.9.11:9000"
+IP_URL = "/ip"
+LOCAL_IP ="1.83.232.70"
+POST_TEST_URL = "/post"
